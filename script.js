2236a4177a53fc775f08d7582e3394a4cc7243e9
Aman bhai <rohanrikwar9555@gmail.com>
2023-09-01 13:26:16 +0530

    navbar_done

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ for(let i=0;i<element.length;i++){
  * show the loader until the page load
  */
 
-const preloader=document.querySelector(".preloader");
+const preloader=document.querySelector("[data-preloader]");
 
 
 
@@ -27,3 +27,25 @@ window.addEventListener("load",()=>{
     preloader.classList.add("loaded");
     document.body.classList.add("loaded")
 })
+
+
+
+
+/****
+ * 
+ * close and active overlay in close  menu btn
+ * 
+ */
+
+const navbar=document.querySelector("[data-navbar]");
+const togglers=document.querySelectorAll("[data-nav-toggler]");
+const overlay=document.querySelector("[data-overlay]");
+
+const togglernav=function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+}
+
+addEventonelements(togglers,"click",togglernav)
